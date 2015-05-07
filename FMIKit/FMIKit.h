888d2ff04243fb19dbe62c677475b444869b3f20
Florian Mielke <github@initialwert.de>
2015-05-07 11:11:36 +0200

    Initial Commit

---
+++ FMIKit/FMIKit.h
@@ -0,0 +1,19 @@
+//
+//  FMIKit.h
+//  FMIKit
+//
+//  Created by Florian Mielke on 07.05.15.
+//  Copyright (c) 2015 madeFM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FMIKit.
+FOUNDATION_EXPORT double FMIKitVersionNumber;
+
+//! Project version string for FMIKit.
+FOUNDATION_EXPORT const unsigned char FMIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FMIKit/PublicHeader.h>
+
+
