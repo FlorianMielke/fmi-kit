181371ddc984d69d59bd4777582b664ee0b342ce
Florian Mielke <github@initialwert.de>
2015-09-10 16:51:13 +0200

    Code revision of NSCalendar+SharedInstances

--- FMIKit/Categories/NSCalendar+SharedInstances.h
+++ FMIKit/Categories/NSCalendar+SharedInstances.h
@@ -1,17 +1,18 @@
 //
-//  NSCalendar+SharedInstances.h
-//
 //  Created by Florian Mielke on 12.11.12.
 //  Copyright (c) 2012 Florian Mielke. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
+NS_ASSUME_NONNULL_BEGIN
 
 @interface NSCalendar (SharedInstances)
 
-+ (id)sharedCurrentCalendar;
++ (NSCalendar *)sharedCurrentCalendar;
 
-+ (id)sharedGMTCalendar;
++ (NSCalendar *)sharedGMTCalendar;
 
 @end
+
+NS_ASSUME_NONNULL_END
--- FMIKit/Categories/NSCalendar+SharedInstances.m
+++ FMIKit/Categories/NSCalendar+SharedInstances.m
@@ -1,41 +1,29 @@
 //
-//  NSCalendar+SharedInstances.m
-//
 //  Created by Florian Mielke on 12.11.12.
 //  Copyright (c) 2012 Florian Mielke. All rights reserved.
 //
 
 #import "NSCalendar+SharedInstances.h"
 
-
 @implementation NSCalendar (SharedInstances)
 
-
-+ (id)sharedCurrentCalendar
-{
++ (NSCalendar *)sharedCurrentCalendar {
     static NSCalendar *sharedInstance = nil;
     static dispatch_once_t onceToken;
-    
     dispatch_once(&onceToken, ^{
         sharedInstance = [NSCalendar currentCalendar];
     });
-    
     return sharedInstance;
 }
 
-
-+ (id)sharedGMTCalendar
-{
++ (NSCalendar *)sharedGMTCalendar {
     static NSCalendar *sharedInstance = nil;
     static dispatch_once_t onceToken;
-    
     dispatch_once(&onceToken, ^{
         sharedInstance = [NSCalendar currentCalendar];
         [sharedInstance setTimeZone:[NSTimeZone timeZoneForSecondsFromGMT:0]];
     });
-
     return sharedInstance;
 }
-
 
 @end
