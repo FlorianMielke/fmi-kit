a62c6bf25323f16086145c3646a33b2d300063ff
Florian Mielke <Florian@biffymini.speedport_w_724v_09011603_00_018>
2016-03-09 09:31:05 +0100

    Add time zone and is all day flag to period

--- FMIKit/FMIPeriod.h
+++ FMIKit/FMIPeriod.h
@@ -4,12 +4,14 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface FMIPeriod : NSObject <NSSecureCoding, NSCopying>
 
+@property (readonly, NS_NONATOMIC_IOSONLY) BOOL isAllDay;
 @property (readonly, NS_NONATOMIC_IOSONLY) NSDate *startDate;
 @property (readonly, NS_NONATOMIC_IOSONLY) NSDate *endDate;
+@property (readonly, NS_NONATOMIC_IOSONLY) NSTimeZone *timeZone;
 
 - (instancetype)init NS_UNAVAILABLE;
 
-- (instancetype)initWithStartDate:(NSDate *)startDate endDate:(NSDate *)endDate;
+- (instancetype)initWithStartDate:(NSDate *)startDate endDate:(NSDate *)endDate inTimeZone:(NSTimeZone *)timeZone isAllDay:(BOOL)isAllDay;
 
 @end
 
--- FMIKit/FMIPeriod.m
+++ FMIKit/FMIPeriod.m
@@ -3,17 +3,19 @@
 
 @implementation FMIPeriod
 
-- (instancetype)initWithStartDate:(NSDate *)startDate endDate:(NSDate *)endDate {
+- (instancetype)initWithStartDate:(NSDate *)startDate endDate:(NSDate *)endDate inTimeZone:(NSTimeZone *)timeZone isAllDay:(BOOL)isAllDay {
     self = [super init];
     if (self) {
         _startDate = startDate;
         _endDate = endDate;
+        _isAllDay = isAllDay;
+        _timeZone = timeZone;
     }
     return self;
 }
 
 - (id)copyWithZone:(NSZone *)zone {
-    return [[FMIPeriod alloc] initWithStartDate:[self.startDate copy] endDate:[self.endDate copy]];
+    return [[FMIPeriod alloc] initWithStartDate:[self.startDate copy] endDate:[self.endDate copy] inTimeZone:[self.timeZone copy] isAllDay:self.isAllDay];
 }
 
 - (BOOL)isEqual:(id)other {
@@ -29,16 +31,22 @@
         return YES;
     if (period == nil)
         return NO;
+    if (self.isAllDay != period.isAllDay)
+        return NO;
     if (self.startDate != period.startDate && ![self.startDate isEqualToDate:period.startDate])
         return NO;
     if (self.endDate != period.endDate && ![self.endDate isEqualToDate:period.endDate])
+        return NO;
+    if (self.timeZone != period.timeZone && ![self.timeZone isEqualToTimeZone:period.timeZone])
         return NO;
     return YES;
 }
 
 - (NSUInteger)hash {
-    NSUInteger hash = [self.startDate hash];
+    NSUInteger hash = (NSUInteger) self.isAllDay;
+    hash = hash * 31u + [self.startDate hash];
     hash = hash * 31u + [self.endDate hash];
+    hash = hash * 31u + [self.timeZone hash];
     return hash;
 }
 
@@ -47,6 +55,8 @@
     if (self) {
         FMI_DECODE_OBJ_CLASS(aDecoder, startDate, NSDate);
         FMI_DECODE_OBJ_CLASS(aDecoder, endDate, NSDate);
+        FMI_DECODE_OBJ_CLASS(aDecoder, timeZone, NSTimeZone);
+        FMI_DECODE_BOOL(aDecoder, isAllDay);
     }
     return self;
 }
@@ -54,6 +64,8 @@
 - (void)encodeWithCoder:(NSCoder *)aCoder {
     FMI_ENCODE_OBJ(aCoder, startDate);
     FMI_ENCODE_OBJ(aCoder, endDate);
+    FMI_ENCODE_OBJ(aCoder, timeZone);
+    FMI_ENCODE_BOOL(aCoder, isAllDay);
 }
 
 + (BOOL)supportsSecureCoding {
@@ -64,6 +76,8 @@
     NSMutableString *description = [NSMutableString stringWithFormat:@"<%@: ", NSStringFromClass([self class])];
     [description appendFormat:@"startDate=%@", self.startDate];
     [description appendFormat:@"endDate=%@", self.endDate];
+    [description appendFormat:@"timeZone=%@", self.timeZone.name];
+    [description appendFormat:@"isAllDay=%@", @(self.isAllDay)];
     [description appendString:@">"];
     return description;
 }
