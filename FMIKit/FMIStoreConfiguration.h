6cbf89b4393f8d9a6f53361ca3e7398f516c278b
Florian Mielke <github@initialwert.de>
2016-05-19 16:35:07 +0200

    We can now determine the current store URL and options from the store configuration

--- FMIKit/FMIStoreConfiguration.h
+++ FMIKit/FMIStoreConfiguration.h
@@ -9,16 +9,24 @@
 #import <Foundation/Foundation.h>
 #import "FMICloudStatus.h"
 
+@class FMIFetchCloudStatus;
+
+NS_ASSUME_NONNULL_BEGIN
+
 @interface FMIStoreConfiguration : NSObject
 
 @property (readonly, NS_NONATOMIC_IOSONLY) NSURL *localStoreURL;
+@property (readonly, copy, NS_NONATOMIC_IOSONLY) NSDictionary *localStoreOptions;
 @property (readonly, NS_NONATOMIC_IOSONLY) NSURL *cloudStoreURL;
-@property (readonly, NS_NONATOMIC_IOSONLY) NSDictionary *localStoreOptions;
-@property (readonly, NS_NONATOMIC_IOSONLY) NSDictionary *cloudStoreOptions;
+@property (readonly, copy, NS_NONATOMIC_IOSONLY) NSDictionary *cloudStoreOptions;
 @property (readonly, NS_NONATOMIC_IOSONLY) NSURL *managedObjectModelURL;
+@property (readonly, copy, NS_NONATOMIC_IOSONLY) NSDictionary *currentStoreOptions;
+@property (readonly, NS_NONATOMIC_IOSONLY) NSURL *currentStoreURL;
 
 - (instancetype)init NS_UNAVAILABLE;
 
-- (instancetype)initWithManagedObjectModelURL:(NSURL *)managedObjectModelURL localStoreURL:(NSURL *)localStoreURL localStoreOptions:(NSDictionary *)localStoreOptions cloudStoreURL:(NSURL *)cloudStoreURL cloudStoreOptions:(NSDictionary *)cloudStoreOptions;
+- (instancetype)initWithManagedObjectModelURL:(NSURL *)managedObjectModelURL fetchCloudStatus:(FMIFetchCloudStatus *)fetchCloudStatus localStoreURL:(NSURL *)localStoreURL localStoreOptions:(NSDictionary *)localStoreOptions cloudStoreURL:(NSURL *)cloudStoreURL cloudStoreOptions:(NSDictionary *)cloudStoreOptions;
 
 @end
+
+NS_ASSUME_NONNULL_END
--- FMIKit/FMIStoreConfiguration.m
+++ FMIKit/FMIStoreConfiguration.m
@@ -7,19 +7,40 @@
 //
 
 #import "FMIStoreConfiguration.h"
+#import "FMIFetchCloudStatus.h"
+
+@interface FMIStoreConfiguration ()
+
+@property (NS_NONATOMIC_IOSONLY) NSURL *localStoreURL;
+@property (NS_NONATOMIC_IOSONLY) NSURL *cloudStoreURL;
+@property (NS_NONATOMIC_IOSONLY) NSURL *managedObjectModelURL;
+@property (copy, NS_NONATOMIC_IOSONLY) NSDictionary *cloudStoreOptions;
+@property (copy, NS_NONATOMIC_IOSONLY) NSDictionary *localStoreOptions;
+@property (NS_NONATOMIC_IOSONLY) FMIFetchCloudStatus *fetchCloudStatus;
+
+@end
 
 @implementation FMIStoreConfiguration
 
-- (instancetype)initWithManagedObjectModelURL:(NSURL *)managedObjectModelURL localStoreURL:(NSURL *)localStoreURL localStoreOptions:(NSDictionary *)localStoreOptions cloudStoreURL:(NSURL *)cloudStoreURL cloudStoreOptions:(NSDictionary *)cloudStoreOptions {
+- (instancetype)initWithManagedObjectModelURL:(NSURL *)managedObjectModelURL fetchCloudStatus:(FMIFetchCloudStatus *)fetchCloudStatus localStoreURL:(NSURL *)localStoreURL localStoreOptions:(NSDictionary *)localStoreOptions cloudStoreURL:(NSURL *)cloudStoreURL cloudStoreOptions:(NSDictionary *)cloudStoreOptions {
     self = [super init];
     if (self) {
-        _localStoreURL = localStoreURL;
-        _cloudStoreURL = cloudStoreURL;
-        _managedObjectModelURL = managedObjectModelURL;
-        _cloudStoreOptions = cloudStoreOptions;
-        _localStoreOptions = localStoreOptions;
+        self.localStoreURL = localStoreURL;
+        self.cloudStoreURL = cloudStoreURL;
+        self.managedObjectModelURL = managedObjectModelURL;
+        self.cloudStoreOptions = cloudStoreOptions;
+        self.localStoreOptions = localStoreOptions;
+        self.fetchCloudStatus = fetchCloudStatus;
     }
     return self;
+}
+
+- (NSURL *)currentStoreURL {
+    return ([self.fetchCloudStatus fetchCloudStatus] == FMICloudStatusEnabled) ? self.cloudStoreURL  : self.localStoreURL;
+}
+
+- (NSDictionary *)currentStoreOptions {
+    return ([self.fetchCloudStatus fetchCloudStatus] == FMICloudStatusEnabled) ? [self.cloudStoreOptions copy] : [self.localStoreOptions copy];
 }
 
 @end
