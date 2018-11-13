a8e36e147d108460869c1a9fdb1653daaa187418
Florian Mielke <f.mielke@systemweit.de>
2018-11-13 16:41:30 +0100

    Add extension

---
+++ FMIKit/Identifiable.swift
@@ -0,0 +1,9 @@
+public protocol Identifiable {
+    static var identifier: String { get }
+}
+
+public extension Identifiable {
+    static var identifier: String {
+        return String(describing: self)
+    }
+}
---
+++ FMIKit/IndexPath+Zero.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+public extension IndexPath {
+    static var zero: IndexPath {
+        return IndexPath(row: 0, section: 0)
+    }
+}
