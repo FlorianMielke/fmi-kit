d16b3b7780b34184ca964c29316b08734f940c0b
Florian Mielke <f.mielke@systemweit.de>
2019-02-19 09:14:42 +0100

    Introduce AnyError

--- FMIKit/Result.swift
+++ FMIKit/Result.swift
@@ -1,4 +1,16 @@
-public enum Result<Value, Error: Swift.Error> {
-  case success(Value)
-  case failure(Error)
+public enum Result<Success, Failure: Error> {
+  case success(Success)
+  case failure(Failure)
+}
+
+public struct AnyError: Swift.Error, CustomStringConvertible {
+  public let underlyingError: Swift.Error
+  
+  public init(_ error: Swift.Error) {
+    self.underlyingError = error
+  }
+  
+  public var description: String {
+    return String(describing: underlyingError)
+  }
 }
