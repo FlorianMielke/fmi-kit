d9348bf752b56418e0c72fa33c9298bd1d097348
Florian Mielke <github@initialwert.de>
2019-03-31 17:43:30 +0200

    We can now provide a default font when changing font size

--- FMIKit/NSAttributedString+FontSize.swift
+++ FMIKit/NSAttributedString+FontSize.swift
@@ -1,16 +1,17 @@
 extension NSAttributedString {
-  @objc public func changeTo(fontSize: CGFloat) -> NSAttributedString {
+  @objc public func changeTo(fontSize: CGFloat, defaultFont: UIFont?) -> NSAttributedString {
     guard let output = self.mutableCopy() as? NSMutableAttributedString else {
       return self
     }
     output.beginEditing()
     output.enumerateAttribute(NSAttributedString.Key.font, in: NSRange(location: 0, length: self.length), options: []) { (value, range, stop) -> Void in
-      guard let oldFont = value as? UIFont else {
-        return
+      if let oldFont = value as? UIFont {
+        let newFont = oldFont.withSize(fontSize)
+        output.removeAttribute(NSAttributedString.Key.font, range: range)
+        output.addAttribute(NSAttributedString.Key.font, value: newFont, range: range)
+      } else if let defaultFont = defaultFont {
+        output.addAttribute(NSAttributedString.Key.font, value: defaultFont, range: range)
       }
-      let newFont = oldFont.withSize(fontSize)
-      output.removeAttribute(NSAttributedString.Key.font, range: range)
-      output.addAttribute(NSAttributedString.Key.font, value: newFont, range: range)
     }
     output.endEditing()
     return output
