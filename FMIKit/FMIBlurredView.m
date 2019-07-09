f035cc5625e49a601f4eeb2468f802845e24105e
Florian Mielke <florian@systemweit.de>
2019-07-09 15:21:29 +0200

    Format code

--- FMIKit/FMIBlurredView.m
+++ FMIKit/FMIBlurredView.m
@@ -13,66 +13,66 @@
 @implementation FMIBlurredView
 
 - (instancetype)initWithCoder:(NSCoder *)aDecoder {
-    self = [super initWithCoder:aDecoder];
-    if (self) {
-        self.translatesAutoresizingMaskIntoConstraints = NO;
-        self.initialBackgroundColor = self.backgroundColor;
-        self.showBlurredBackground = YES;
-        self.showTopHairline = NO;
-        self.showBottomHairline = NO;
-        self.blurredView = [self prepareVisualEffectView];
-        [self insertSubview:self.blurredView atIndex:0];
-        UIVisualEffectView *blurredView = self.blurredView;
-        [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[blurredView]-0-|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(blurredView)]];
-        [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[blurredView]-0-|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(blurredView)]];
-
-        UIColor *hairlineColor = [UIColor colorWithRed:0.737 green:0.729 blue:0.757 alpha:1.0];
-        self.topHairline = [[FMIHairlineView alloc] initWithLineColor:hairlineColor];
-        [self insertSubview:self.topHairline atIndex:1];
-        [self layoutTopHairline];
-
-        self.bottomHairline = [[FMIHairlineView alloc] initWithLineColor:hairlineColor];
-        [self insertSubview:self.bottomHairline atIndex:1];
-        [self layoutBottomHairline];
-    }
-    return self;
+  self = [super initWithCoder:aDecoder];
+  if (self) {
+    self.translatesAutoresizingMaskIntoConstraints = NO;
+    self.initialBackgroundColor = self.backgroundColor;
+    self.showBlurredBackground = YES;
+    self.showTopHairline = NO;
+    self.showBottomHairline = NO;
+    self.blurredView = [self prepareVisualEffectView];
+    [self insertSubview:self.blurredView atIndex:0];
+    UIVisualEffectView *blurredView = self.blurredView;
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[blurredView]-0-|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(blurredView)]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[blurredView]-0-|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(blurredView)]];
+    
+    UIColor *hairlineColor = [UIColor colorWithRed:0.737 green:0.729 blue:0.757 alpha:1.0];
+    self.topHairline = [[FMIHairlineView alloc] initWithLineColor:hairlineColor];
+    [self insertSubview:self.topHairline atIndex:1];
+    [self layoutTopHairline];
+    
+    self.bottomHairline = [[FMIHairlineView alloc] initWithLineColor:hairlineColor];
+    [self insertSubview:self.bottomHairline atIndex:1];
+    [self layoutBottomHairline];
+  }
+  return self;
 }
 
 - (void)layoutTopHairline {
-    NSLayoutConstraint *leftConstraint = [NSLayoutConstraint constraintWithItem:self attribute:NSLayoutAttributeLeft relatedBy:NSLayoutRelationEqual toItem:self.topHairline attribute:NSLayoutAttributeLeft multiplier:1.0 constant:0.0];
-    NSLayoutConstraint *rightConstraint = [NSLayoutConstraint constraintWithItem:self attribute:NSLayoutAttributeRight relatedBy:NSLayoutRelationEqual toItem:self.topHairline attribute:NSLayoutAttributeRight multiplier:1.0 constant:0.0];
-    NSLayoutConstraint *topConstraint = [NSLayoutConstraint constraintWithItem:self attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.topHairline attribute:NSLayoutAttributeTop multiplier:1.0 constant:0.0];
-    [self addConstraints:@[topConstraint, leftConstraint, rightConstraint]];
+  NSLayoutConstraint *leftConstraint = [NSLayoutConstraint constraintWithItem:self attribute:NSLayoutAttributeLeft relatedBy:NSLayoutRelationEqual toItem:self.topHairline attribute:NSLayoutAttributeLeft multiplier:1.0 constant:0.0];
+  NSLayoutConstraint *rightConstraint = [NSLayoutConstraint constraintWithItem:self attribute:NSLayoutAttributeRight relatedBy:NSLayoutRelationEqual toItem:self.topHairline attribute:NSLayoutAttributeRight multiplier:1.0 constant:0.0];
+  NSLayoutConstraint *topConstraint = [NSLayoutConstraint constraintWithItem:self attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.topHairline attribute:NSLayoutAttributeTop multiplier:1.0 constant:0.0];
+  [self addConstraints:@[topConstraint, leftConstraint, rightConstraint]];
 }
 
 - (void)layoutBottomHairline {
-    NSLayoutConstraint *leftConstraint = [NSLayoutConstraint constraintWithItem:self attribute:NSLayoutAttributeLeft relatedBy:NSLayoutRelationEqual toItem:self.bottomHairline attribute:NSLayoutAttributeLeft multiplier:1.0 constant:0.0];
-    NSLayoutConstraint *rightConstraint = [NSLayoutConstraint constraintWithItem:self attribute:NSLayoutAttributeRight relatedBy:NSLayoutRelationEqual toItem:self.bottomHairline attribute:NSLayoutAttributeRight multiplier:1.0 constant:0.0];
-    NSLayoutConstraint *bottomConstraint = [NSLayoutConstraint constraintWithItem:self attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self.bottomHairline attribute:NSLayoutAttributeBottom multiplier:1.0 constant:0.0];
-    [self addConstraints:@[bottomConstraint, leftConstraint, rightConstraint]];
+  NSLayoutConstraint *leftConstraint = [NSLayoutConstraint constraintWithItem:self attribute:NSLayoutAttributeLeft relatedBy:NSLayoutRelationEqual toItem:self.bottomHairline attribute:NSLayoutAttributeLeft multiplier:1.0 constant:0.0];
+  NSLayoutConstraint *rightConstraint = [NSLayoutConstraint constraintWithItem:self attribute:NSLayoutAttributeRight relatedBy:NSLayoutRelationEqual toItem:self.bottomHairline attribute:NSLayoutAttributeRight multiplier:1.0 constant:0.0];
+  NSLayoutConstraint *bottomConstraint = [NSLayoutConstraint constraintWithItem:self attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self.bottomHairline attribute:NSLayoutAttributeBottom multiplier:1.0 constant:0.0];
+  [self addConstraints:@[bottomConstraint, leftConstraint, rightConstraint]];
 }
 
 - (UIVisualEffectView *)prepareVisualEffectView {
-    UIBlurEffect *effect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleLight];
-    UIVisualEffectView *view = [[UIVisualEffectView alloc] initWithEffect:effect];
-    view.translatesAutoresizingMaskIntoConstraints = NO;
-    return view;
+  UIBlurEffect *effect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleLight];
+  UIVisualEffectView *view = [[UIVisualEffectView alloc] initWithEffect:effect];
+  view.translatesAutoresizingMaskIntoConstraints = NO;
+  return view;
 }
 
 - (void)setShowBlurredBackground:(BOOL)showBlurredBackground {
-    _showBlurredBackground = showBlurredBackground;
-    self.blurredView.hidden = !_showBlurredBackground;
-    self.backgroundColor = (_showBlurredBackground) ? [UIColor clearColor] : self.initialBackgroundColor;
+  _showBlurredBackground = showBlurredBackground;
+  self.blurredView.hidden = !_showBlurredBackground;
+  self.backgroundColor = (_showBlurredBackground) ? [UIColor clearColor] : self.initialBackgroundColor;
 }
 
 - (void)setShowTopHairline:(BOOL)showTopHairline {
-    _showTopHairline = showTopHairline;
-    self.topHairline.hidden = !_showTopHairline;
+  _showTopHairline = showTopHairline;
+  self.topHairline.hidden = !_showTopHairline;
 }
 
 - (void)setShowBottomHairline:(BOOL)showBottomHairline {
-    _showBottomHairline = showBottomHairline;
-    self.bottomHairline.hidden = !_showBottomHairline;
+  _showBottomHairline = showBottomHairline;
+  self.bottomHairline.hidden = !_showBottomHairline;
 }
 
 @end
