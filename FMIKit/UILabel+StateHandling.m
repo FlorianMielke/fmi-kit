a9369bfcc3c490cc5678c01d390b1b8f69a5415f
Florian Mielke <github@initialwert.de>
2020-02-13 16:01:57 +0100

    Fix UILabel is valid state

--- FMIKit/UILabel+StateHandling.m
+++ FMIKit/UILabel+StateHandling.m
@@ -1,42 +1,25 @@
-//
-//  UILabel+StateHandling.m
-//
-//  Created by Florian Mielke on 18.10.13.
-//  Copyright (c) 2013 Florian Mielke. All rights reserved.
-//
-
 #import "UILabel+StateHandling.h"
 #import "UIColor+SystemDefaults.h"
 
-
 @implementation UILabel (StateHandling)
 
-
-- (void)fm_markAsActive:(BOOL)active
-{
-    self.textColor = (active) ? self.tintColor : [UIColor cellDefaultStyleDetailLabelColor];
+- (void)fm_markAsActive:(BOOL)active {
+  self.textColor = (active) ? self.tintColor : [UIColor cellDefaultStyleDetailLabelColor];
 }
 
-
-- (void)fm_markAsValid:(BOOL)valid
-{
-    NSMutableDictionary *attributes = [[self.attributedText attributesAtIndex:0 effectiveRange:NULL] mutableCopy];
-
-    if (!valid) {
-        [attributes setValue:@(NSUnderlineStyleSingle) forKey:NSStrikethroughStyleAttributeName];
-    } else {
-        [attributes removeObjectForKey:NSStrikethroughStyleAttributeName];
-    }
-    
-    self.attributedText = [[NSAttributedString alloc] initWithString:self.text attributes:[attributes copy]];
+- (void)fm_markAsValid:(BOOL)valid {
+  NSMutableDictionary *attributes = [[self.attributedText attributesAtIndex:0 effectiveRange:NULL] mutableCopy];
+  if (valid) {
+    [attributes setValue:@(NSUnderlineStyleNone) forKey:NSStrikethroughStyleAttributeName];
+  } else {
+    [attributes setValue:@(NSUnderlineStyleSingle) forKey:NSStrikethroughStyleAttributeName];
+  }
+  self.attributedText = [[NSAttributedString alloc] initWithString:self.text attributes:[attributes copy]];
 }
 
-
-- (void)fm_setAttributedText:(NSString *)text
-{
-    NSDictionary *attributes = [self.attributedText attributesAtIndex:0 effectiveRange:NULL];
-    self.attributedText = [[NSAttributedString alloc] initWithString:text attributes:attributes];
+- (void)fm_setAttributedText:(NSString *)text {
+  NSDictionary *attributes = [self.attributedText attributesAtIndex:0 effectiveRange:NULL];
+  self.attributedText = [[NSAttributedString alloc] initWithString:text attributes:attributes];
 }
-
 
 @end
