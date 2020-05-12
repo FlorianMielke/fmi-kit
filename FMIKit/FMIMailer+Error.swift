b30e708e050a3650050dc943a1cdf33a34a66ff3
Florian Mielke <github@initialwert.de>
2020-05-12 12:53:51 +0200

    Make method available public

--- FMIKit/FMIMailer+Error.swift
+++ FMIKit/FMIMailer+Error.swift
@@ -1,5 +1,5 @@
-extension FMIMailer {
-    public func mail<Error>(error: Error, presentingViewController viewController: UIViewController & FMIMailerDelegate, emailAddress: String) where Error: LocalizedError {
+public extension FMIMailer {
+    func mail<Error>(error: Error, presentingViewController viewController: UIViewController & FMIMailerDelegate, emailAddress: String) where Error: LocalizedError {
         let errorMessage = FMIKitFactory.createErrorMessage(forError: error, bundle: Bundle.main, emailAddress: emailAddress)
         mail(errorMessage, withPresenting: viewController, delegate: viewController)
     }
