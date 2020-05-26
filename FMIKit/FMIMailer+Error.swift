f17428d187733e1a727d1df12a13ea53429ac11b
Florian Mielke <github@initialwert.de>
2020-05-26 12:11:39 +0200

    Generalize FMIMailer extension for errors

--- FMIKit/FMIMailer+Error.swift
+++ FMIKit/FMIMailer+Error.swift
@@ -1,5 +1,5 @@
 public extension FMIMailer {
-    func mail<Error>(error: Error, presentingViewController viewController: UIViewController & FMIMailerDelegate, emailAddress: String) where Error: LocalizedError {
+    @objc func mail(error: Error, presentingViewController viewController: UIViewController & FMIMailerDelegate, emailAddress: String) {
         let errorMessage = FMIKitFactory.createErrorMessage(forError: error, bundle: Bundle.main, emailAddress: emailAddress)
         mail(errorMessage, withPresenting: viewController, delegate: viewController)
     }
