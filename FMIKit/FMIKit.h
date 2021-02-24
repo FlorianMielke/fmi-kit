9e60b69e957e5666876260dfa07b90a37faec175
Florian Mielke <github@initialwert.de>
2021-02-24 16:59:00 +0100

    Remove NestedTableView

--- FMIKit/FMIKit.h
+++ FMIKit/FMIKit.h
@@ -91,7 +91,6 @@ FOUNDATION_EXPORT const unsigned char FMIKitVersionString[];
 #import <FMIKit/FMIEditingToolbar.h>
 #import <FMIKit/FMIEventParser.h>
 #import <FMIKit/FMIHairlineView.h>
-#import <FMIKit/FMINestedTableView.h>
 #import <FMIKit/FMINonAminatedPushSegue.h>
 #import <FMIKit/FMINumericTextView.h>
 #import <FMIKit/FMITableView.h>
