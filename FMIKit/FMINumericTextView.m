5ced1f08ab08c52e6727582f29f1a8d125dfcbb7
Florian Mielke <github@initialwert.de>
2021-04-07 12:14:58 +0200

    Format code

--- FMIKit/FMINumericTextView.m
+++ FMIKit/FMINumericTextView.m
@@ -24,22 +24,18 @@
 
 #pragma mark - Initialization
 
-- (instancetype)init
-{
+- (instancetype)init {
     return [self initWithAccessoryButtonType:FMINumericTextViewAccessoryButtonTypeNone notificationCenter:[NSNotificationCenter defaultCenter]];
 }
 
-- (instancetype)initWithAccessoryButtonType:(FMINumericTextViewAccessoryButtonType)accessoryButtonType
-{
+- (instancetype)initWithAccessoryButtonType:(FMINumericTextViewAccessoryButtonType)accessoryButtonType {
     return [self initWithAccessoryButtonType:accessoryButtonType notificationCenter:[NSNotificationCenter defaultCenter]];
 }
 
-- (instancetype)initWithAccessoryButtonType:(FMINumericTextViewAccessoryButtonType)accessoryButtonType notificationCenter:(NSNotificationCenter *)notificationCenter
-{
+- (instancetype)initWithAccessoryButtonType:(FMINumericTextViewAccessoryButtonType)accessoryButtonType notificationCenter:(NSNotificationCenter *)notificationCenter {
     self = [super init];
     
-    if (self != nil)
-    {
+    if (self != nil) {
         self.durationStyle = FMIDurationFormatterStyleTime;
         self.accessoryButtonType = accessoryButtonType;
         self.notificationCenter = notificationCenter;
@@ -49,19 +45,12 @@
     return self;
 }
 
-- (UIView *)inputAccessoryView
-{
-    if (!_accessoryView)
-    {
+- (UIView *)inputAccessoryView {
+    if (_accessoryView == nil) {
         _accessoryView = [[UIToolbar alloc] initWithFrame:CGRectMake(0.0, 0.0, 0.0, 44.0)];
         _accessoryView.autoresizingMask = UIViewAutoresizingFlexibleWidth;
-      if (@available(iOS 13.0, *)) {
-        _accessoryView.tintColor = [UIColor labelColor];
-        _accessoryView.barTintColor = [UIColor systemBackgroundColor];
-      } else {
         _accessoryView.tintColor = [UIColor whiteColor];
         _accessoryView.barTintColor = [UIColor colorWithRed:0.608 green:0.639 blue:0.690 alpha:1.0];
-      }
         _accessoryView.delegate = self;
         
         UIBarButtonItem *fixedButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFixedSpace target:nil action:nil];
@@ -73,15 +62,13 @@
     return _accessoryView;
 }
 
-- (UIBarPosition)positionForBar:(id<UIBarPositioning>)bar
-{
+- (UIBarPosition)positionForBar:(id<UIBarPositioning>)bar {
     return UIBarPositionAny;
 }
 
 #pragma mark - Toggle keyboard
 
-- (void)toggleKeyboard
-{
+- (void)toggleKeyboard {
     self.showsKeyboard = !(self.showsKeyboard);
     [self askDelegateAboutBalancedToggleForCompoundTextViewsIfNeeded];
     
@@ -92,36 +79,29 @@
     }
 }
 
-- (void)askDelegateAboutBalancedToggleForCompoundTextViewsIfNeeded
-{
-    if (self.showsKeyboard && self.delegate != nil && [self.delegate respondsToSelector:@selector(compoundTextViewsForTextView:)])
-    {
+- (void)askDelegateAboutBalancedToggleForCompoundTextViewsIfNeeded {
+    if (self.showsKeyboard && self.delegate != nil && [self.delegate respondsToSelector:@selector(compoundTextViewsForTextView:)]) {
         NSArray *compoundTextViews = [self.delegate compoundTextViewsForTextView:self];
         [compoundTextViews enumerateObjectsUsingBlock:^(FMINumericTextView *compoundTextView, NSUInteger idx, BOOL *stop) {
-            
             if (![compoundTextView isEqual:self] && compoundTextView.showsKeyboard) {
                 compoundTextView.showsKeyboard = NO;
             }
-            
         }];
     }
 }
 
 #pragma mark - Clear text
 
-- (IBAction)clearText:(id)sender
-{
+- (IBAction)clearText:(id)sender {
     self.text = [self clearedText];
     [self informDelegateAboutClearingText];
 }
 
-- (NSString *)clearedText
-{
+- (NSString *)clearedText {
     return ([self.text hasPrefix:@"-"]) ? @"-" : @"";
 }
 
-- (void)informDelegateAboutClearingText
-{
+- (void)informDelegateAboutClearingText {
     if (self.delegate != nil && [self.delegate respondsToSelector:@selector(textViewDidClearText:)]) {
         [self.delegate textViewDidClearText:self];
     }
@@ -129,19 +109,16 @@
 
 #pragma mark - Invert text
 
-- (IBAction)invertText:(id)sender
-{
+- (IBAction)invertText:(id)sender {
     self.text = [self invertedText];
     [self informDelegateAboutInvertingText];
 }
 
-- (NSString *)invertedText
-{
+- (NSString *)invertedText {
     return ([self.text hasPrefix:@"-"]) ? [self.text substringFromIndex:1] : [NSString stringWithFormat:@"-%@", self.text];
 }
 
-- (void)informDelegateAboutInvertingText
-{
+- (void)informDelegateAboutInvertingText {
     if (self.delegate != nil && [self.delegate respondsToSelector:@selector(textViewDidInvertText:)]) {
         [self.delegate textViewDidInvertText:self];
     }
@@ -149,33 +126,28 @@
 
 #pragma mark - Keyboard notifications
 
-- (BOOL)becomeFirstResponder
-{
+- (BOOL)becomeFirstResponder {
     [self registerForKeyboardAppearanceNotification];
     return [super becomeFirstResponder];
 }
 
-- (BOOL)resignFirstResponder
-{
+- (BOOL)resignFirstResponder {
     self.showsKeyboard = NO;
     return [super resignFirstResponder];
 }
 
-- (void)keyboardWillHide:(NSNotification *)notification
-{
-    [self removeFromKeyboardAppearanceNotitication];
+- (void)keyboardWillHide:(NSNotification *)notification {
+    [self removeFromKeyboardAppearanceNotification];
     self.showsKeyboard = NO;
 }
 
-- (void)registerForKeyboardAppearanceNotification
-{
+- (void)registerForKeyboardAppearanceNotification {
     if (self.accessoryButtonType != FMINumericTextViewAccessoryButtonTypeNone) {
         [self.notificationCenter addObserver:self selector:@selector(keyboardWillHide:) name:UIKeyboardWillHideNotification object:nil];
     }
 }
 
-- (void)removeFromKeyboardAppearanceNotitication
-{
+- (void)removeFromKeyboardAppearanceNotification {
     if (self.accessoryButtonType != FMINumericTextViewAccessoryButtonTypeNone) {
         [self.notificationCenter removeObserver:self name:UIKeyboardWillHideNotification object:nil];
     }
@@ -183,29 +155,25 @@
 
 #pragma mark - Prepare clear button
 
-- (UIBarButtonItem *)accessoryButton
-{
-    if (!_accessoryButton && self.accessoryButtonType != FMINumericTextViewAccessoryButtonTypeNone) {
+- (UIBarButtonItem *)accessoryButton {
+    if (_accessoryButton == nil && self.accessoryButtonType != FMINumericTextViewAccessoryButtonTypeNone) {
         _accessoryButton = (self.accessoryButtonType == FMINumericTextViewAccessoryButtonTypeClear) ? [self clearButton] : [self invertButton];
     }
     
     return _accessoryButton;
 }
 
-- (UIBarButtonItem *)clearButton
-{
+- (UIBarButtonItem *)clearButton {
     FMIDurationFormatter *durationFormatter = [[FMIDurationFormatter alloc] init];
     durationFormatter.style = self.durationStyle;
     return [self accessoryButtonWithTitle:[durationFormatter stringFromDuration:[FMIDuration zero]] action:@selector(clearText:)];
 }
 
-- (UIBarButtonItem *)invertButton
-{
+- (UIBarButtonItem *)invertButton {
     return [self accessoryButtonWithTitle:@"+/-" action:@selector(invertText:)];
 }
 
-- (UIBarButtonItem *)accessoryButtonWithTitle:(NSString *)title action:(SEL)selector
-{
+- (UIBarButtonItem *)accessoryButtonWithTitle:(NSString *)title action:(SEL)selector {
     return [[UIBarButtonItem alloc] initWithTitle:title style:UIBarButtonItemStylePlain target:self action:selector];
 }
 
