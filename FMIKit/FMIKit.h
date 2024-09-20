4752043233ff1e5a4f1f35eb128a4cfbf9b44032
Florian Mielke <github@initialwert.de>
2024-09-20 21:35:16 +0200

    Remove obsolete error classes

--- FMIKit/FMIKit.h
+++ FMIKit/FMIKit.h
@@ -13,16 +13,8 @@ FOUNDATION_EXPORT const unsigned char FMIKitVersionString[];
 
 #import <FMIKit/FMIBinding.h>
 #import <FMIKit/FMIBindingManager.h>
-#import <FMIKit/FMIComparator.h>
 #import <FMIKit/FMIDateHelper.h>
 #import <FMIKit/FMIGMTDateHelper.h>
-#import <FMIKit/FMITimeZoneHelper.h>
-#import <FMIKit/FMIUUIDHelper.h>
-
-#import <FMIKit/FMIYear.h>
-#import <FMIKit/FMIPeriod.h>
-#import <FMIKit/FMIDuration.h>
-#import <FMIKit/FMIDurationFormatter.h>
 
 #pragma mark - Common
 
@@ -32,14 +24,6 @@ FOUNDATION_EXPORT const unsigned char FMIKitVersionString[];
 
 #import <FMIKit/UIDevice+Platform.h>
 #import <FMIKit/UITableView+IndexPath.h>
-
-#import <FMIKit/FMIAlertController.h>
-#import <FMIKit/FMITableView.h>
-
-#pragma mark - Messages
-
-#import <FMIKit/FMIAttachment.h>
-#import <FMIKit/FMIErrorMessage.h>
 
 #pragma mark - Common
 
